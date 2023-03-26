6ad2ed182d04b23c553488b76b8c0184746d3009
Your Name <you@example.com>
2023-03-25 20:52:51 -0500

    making more changes to nav

--- SideNavbar/navbar.js
+++ SideNavbar/navbar.js
@@ -1,5 +1,18 @@
-const toggleBtn = document.querySelector('.toggle-theme');
+function toggleMenu() {
+  document.querySelector(".menu-container").classList.toggle("active");
+}
 
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
+// Get the menu container and the bars icon
+const menuContainer = document.querySelector('.menu-container');
+const barsIcon = document.querySelector('.bars-icon');
+
+// Listen for clicks on the document
+document.addEventListener('click', function (event) {
+  const isClickInsideMenu = menuContainer.contains(event.target);
+  const isClickOnBarsIcon = barsIcon.contains(event.target);
+
+  if (!isClickInsideMenu && !isClickOnBarsIcon && menuContainer.classList.contains('active')) {
+    // Clicked outside the menu container and the bars icon, close it
+    menuContainer.classList.remove('active');
+  }
+}, true);
