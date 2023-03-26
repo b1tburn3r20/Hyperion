4d9a085604d697a103f1a3a04da65fa355660984
Your Name <you@example.com>
2023-03-26 18:20:31 -0500

    Responsive navbar integrated

--- SideNavbar/navbar.js
+++ SideNavbar/navbar.js
@@ -1,18 +1,25 @@
-function toggleMenu() {
-  document.querySelector(".menu-container").classList.toggle("active");
-}
+// navbar.js
+function initNavbar() {
+  // Get the menu container and the bars icon
+  const menuContainer = document.querySelector('.menu-container');
+  const barsIcon = document.querySelector('.bars-icon');
 
-// Get the menu container and the bars icon
-const menuContainer = document.querySelector('.menu-container');
-const barsIcon = document.querySelector('.bars-icon');
-
-// Listen for clicks on the document
-document.addEventListener('click', function (event) {
-  const isClickInsideMenu = menuContainer.contains(event.target);
-  const isClickOnBarsIcon = barsIcon.contains(event.target);
-
-  if (!isClickInsideMenu && !isClickOnBarsIcon && menuContainer.classList.contains('active')) {
-    // Clicked outside the menu container and the bars icon, close it
-    menuContainer.classList.remove('active');
+  // Function to toggle the menu
+  function toggleMenu() {
+    menuContainer.classList.toggle('active');
   }
-}, true);
+
+  // Function to close the menu when clicked outside
+  function closeMenu(event) {
+    const isClickInsideMenu = menuContainer.contains(event.target);
+    const isClickOnBarsIcon = barsIcon.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnBarsIcon && menuContainer.classList.contains('active')) {
+      menuContainer.classList.remove('active');
+    }
+  }
+
+  // Attach event listeners
+  barsIcon.addEventListener('click', toggleMenu);
+  document.addEventListener('click', closeMenu);
+}
