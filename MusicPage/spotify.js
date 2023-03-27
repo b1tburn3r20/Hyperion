2204f94663e7882e8b9bb77146cc7bf1a944ead4
Your Name <you@example.com>
2023-03-27 16:01:17 -0500

    YES. I GOT THE SEARCH TO WORK

--- MusicPage/spotify.js
+++ MusicPage/spotify.js
@@ -2,6 +2,9 @@ const clientId = "69fcf10c73494a8ab4d6dc0d26e23047";
 const clientSecret = "b1607be32c954efb9956f55993fa2f33";
 const playlistIds = ["7AgjraE8aZYVRmPh2rOyZY", "75o0xrk1ZuVPnjyS1lgbvi", "1GC94kkIjKogZ6VciUDa17"];
 // Function to fetch an access token
+let accessToken; // Define the accessToken variable globally
+
+// Function to fetch an access token
 async function getAccessToken() {
   const response = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
@@ -15,8 +18,6 @@ async function getAccessToken() {
   const data = await response.json();
   return data.access_token;
 }
-
-
 
 
 // Function to fetch playlist data
@@ -64,26 +65,29 @@ function displaySongs(songs, playlistContainer) {
   
 
 // Initialize the playlist
+// Initialize the playlist
 (async function initPlaylists() {
-  const accessToken = await getAccessToken();
+  accessToken = await getAccessToken();
 
   // Fetch and display data for each playlist
   playlistIds.forEach((playlistId, index) => {
     fetchPlaylist(accessToken, playlistId, `#playlist${index + 1}`);
   });
-
-  // Search functionality
-  const searchButton = document.getElementById("search-button");
-  const searchInput = document.getElementById("search-input");
-
-  searchButton.addEventListener("click", async () => {
-    const query = searchInput.value.trim();
-    if (query) {
-      const tracks = await searchTracks(accessToken, query); // Use searchTracks instead of searchAlbums
-      displaySongs(tracks, "#search-results"); // Use displaySongs instead of displayAlbums
-    }
-  });
 })();
+
+// Search functionality
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+
+searchButton.addEventListener("click", async () => {
+  const query = searchInput.value.trim();
+  if (query) {
+    const tracks = await searchTracks(accessToken, query);
+    displaySearchResults(tracks); // Replace displaySongs with displaySearchResults
+  }
+});
+
+
 
 async function searchTracks(accessToken, query) {
   const apiUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track&market=US&limit=10`;
@@ -97,4 +101,28 @@ async function searchTracks(accessToken, query) {
 
   const data = await response.json();
   return data.tracks.items; // Return track items instead of album items
+}
+// New function to display search results in the results-feed div
+function displaySearchResults(songs) {
+  const resultsFeed = document.querySelector(".results-feed");
+  resultsFeed.innerHTML = ""; // Clear the feed
+
+  songs.forEach((song) => {
+    const track = song.track ? song.track : song; // Updated to handle both cases
+
+    const songDiv = document.createElement("div");
+    songDiv.className = "music_song";
+
+    // Add Spotify Play Button
+    const playButton = document.createElement("iframe");
+    playButton.src = `https://open.spotify.com/embed/track/${track.id}`;
+    playButton.width = "300";
+    playButton.height = "80";
+    playButton.frameBorder = "0";
+    playButton.allowTransparency = "true";
+    playButton.allow = "encrypted-media";
+    songDiv.appendChild(playButton);
+
+    resultsFeed.appendChild(songDiv);
+  });
 }
